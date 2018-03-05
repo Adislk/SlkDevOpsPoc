72b7fed7a277a39ea4bed91e587f47e84faaf6b1
Adislk <36727624+Adislk@users.noreply.github.com>
2018-03-05 09:38:07 +0000

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ import java.io.*
 import java.util.*
 public class Hello(
 	Public static void main(String[] args){
-	for (int i-1;i<=10;i++){
+	for (int i=1;i<=10;i++){
 	system.out.println("Hello World..."+i);
 			}
 		}
